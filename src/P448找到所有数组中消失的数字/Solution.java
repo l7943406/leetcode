9b68e5ac227ab1d8cbf16b35b278225882be7b97
muchen <2921068018@qq.com>
2021-02-13 14:12:23 +0800

    new

---
+++ src/P448找到所有数组中消失的数字/Solution.java
@@ -0,0 +1,21 @@
+package P448找到所有数组中消失的数字;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums){
+        List<Integer> ans = new ArrayList<>();
+        boolean[] book = new boolean[nums.length + 1];
+        for(int i : nums){
+            book[i] = true;
+        }
+        for (int i = 1; i < book.length; i++) {
+            if(!book[i]){
+                ans.add(i);
+            }
+        }
+        return ans;
+
+    }
+}

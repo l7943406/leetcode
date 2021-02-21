dc98c87388e1e80cc13f9929b3ff752aa3d561c7
muchen <2921068018@qq.com>
2021-02-21 22:09:44 +0800

    new

--- src/P1438绝对差不超过限制的最长连续子数组/Solution.java
+++ src/P1438绝对差不超过限制的最长连续子数组/Solution.java
@@ -1,23 +1,26 @@
 package P1438绝对差不超过限制的最长连续子数组;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
+import java.util.TreeMap;
 
 class Solution {
     public int longestSubarray(int[] nums, int limit) {
         int ans = 0;
-        //顺序
-        PriorityQueue<Integer> order = new PriorityQueue<>((o1, o2) -> o1 - o2);
-        //逆序
-        PriorityQueue<Integer> reverse = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        TreeMap<Integer,Integer> map = new TreeMap<>();
         int l = 0;
         int r = 0;
         for (r = 0; r < nums.length; r++) {
-            order.offer(nums[r]);
-            reverse.offer(nums[r]);
-            while(reverse.peek() - order.peek() > limit){
-                reverse.remove(nums[l]);
-                order.remove(nums[l]);
+            if(!map.containsKey(nums[r])){
+                map.put(nums[r],1);
+            }else{
+                map.put(nums[r],map.get(nums[r])+1);
+            }
+            while(map.lastKey() - map.firstKey() > limit){
+                int countL = map.get(nums[l]);
+                if(countL == 1){
+                    map.remove(nums[l]);
+                }else{
+                    map.put(nums[l],countL - 1);
+                }
                 l++;
             }
             ans = Math.max(ans,r - l + 1);

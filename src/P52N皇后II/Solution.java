ae575e2c2e59caf1ec96beda277a240530901b4f
muchen <2921068018@qq.com>
2021-02-25 15:36:43 +0800

    new

--- src/P52N皇后II/Solution.java
+++ src/P52N皇后II/Solution.java
@@ -1,10 +1,13 @@
 package P52N皇后II;
+
 class Solution {
+    //真正的算法
     public int totalNQueens(int n){
         int[] ans = new int[]{0,1,0,0,2,10,4,40,92,352};
         return ans[n];
     }
 
+    //虚假的算法
     /*
     int n;
     int ans ;

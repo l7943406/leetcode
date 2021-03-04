2a0a708cd08bad392412f9782a4184d8c6684acb
muchen <2921068018@qq.com>
2021-03-04 20:07:52 +0800

    new

--- src/P354俄罗斯套娃信封问题/Solution.java
+++ src/P354俄罗斯套娃信封问题/Solution.java
@@ -4,24 +4,25 @@ import java.util.Arrays;
 
 class Solution {
     public int maxEnvelopes(int[][] envelopes) {
-        Arrays.sort(envelopes, (o1, o2) -> {
-            if(o1[0] == o2[0]){
-                return o2[1] - o1[1];
-            }else{
-                return o1[0] - o2[0];
-            }
-        });
+        int ans = 1;
         int[] dp = new int[envelopes.length];
         Arrays.fill(dp,1);
-        int ans = 1;
+        Arrays.sort(envelopes,(a,b)->{
+            if(a[0] == b[0]){
+                return b[1] - a[1];
+            }else{
+                return a[0] - b[0];
+            }
+        });
         for (int i = 1; i < envelopes.length; i++) {
             for (int j = 0; j < i; j++) {
                 if(envelopes[i][1] > envelopes[j][1]){
-                    dp[i] = Math.max(dp[i],dp[j]+1);
+                    dp[i] = Math.max(dp[i],dp[j] + 1);
                 }
             }
             ans = Math.max(ans,dp[i]);
         }
+
         return ans;
     }
 }

718d6851ec52bce9da9d4f9b3a69bd8e6abcbe8a
muchen <2921068018@qq.com>
2021-03-13 19:41:13 +0800

    new

---
+++ src/P705设计哈希集合/Solution.java
@@ -0,0 +1,30 @@
+package P705设计哈希集合;
+
+class MyHashSet {
+    boolean[] map;
+    /** Initialize your data structure here. */
+    public MyHashSet() {
+        map = new boolean[1000001];
+    }
+
+    public void add(int key) {
+        map[key] = true;
+    }
+
+    public void remove(int key) {
+        map[key] = false;
+    }
+
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        return map[key];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */

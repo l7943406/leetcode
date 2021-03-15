7556fa7c1d2258876772efbef70f91708c3072db
muchen <2921068018@qq.com>
2021-03-15 20:26:13 +0800

    new

--- src/P706设计哈希映射/Solution.java
+++ src/P706设计哈希映射/Solution.java
@@ -1,28 +1,92 @@
 package P706设计哈希映射;
 
-import java.util.Arrays;
 
 class MyHashMap {
-    int[] map;
+    Node[] node;
+    int prim;
     /** Initialize your data structure here. */
     public MyHashMap() {
-        map = new int[1000001];
-        Arrays.fill(map,-1);
+        prim = 9697;
+        node = new Node[prim];
     }
 
     /** value will always be non-negative. */
     public void put(int key, int value) {
-        map[key] = value;
+        int prKey = key % prim;
+        if(node[prKey] == null){
+            node[prKey] = new Node(key,value,null,null);
+        }else{
+            Node t = node[prKey];
+            if(t.key == key){
+                t.value = value;
+                return;
+            }
+            while(t.next != null){
+                t = t.next;
+                if(t.key == key){
+                    t.value = value;
+                    return;
+                }
+            }
+            t.next = new Node(key,value,t,null);
+        }
     }
 
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     public int get(int key) {
-        return map[key];
+        int prKey = key % prim;
+        Node t = node[prKey];
+        while (t!=null){
+            if(t.key == key){
+                return t.value;
+            }
+            t = t.next;
+        }
+        return -1;
     }
 
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     public void remove(int key) {
-        map[key] = -1;
+        int prKey = key % prim;
+        Node t = node[prKey];
+        if(t == null){
+            return;
+        }
+        if(t.key == key){
+            if(t.next == null){
+                node[prKey] = null;
+                return;
+            }else{
+                t.next.last = null;
+                node[prKey] = t.next;
+                return;
+            }
+        }
+        while (t.next != null){
+            t = t.next;
+            if(t.key == key){
+                t.last.next = t.next;
+                if(t.next != null){
+                    t.next.last = t.last;
+                }
+                return;
+            }
+        }
+    }
+    class Node{
+        int key;
+        int value;
+        Node next;
+        Node last;
+        public Node(){
+
+        }
+        public Node(int key,int value,Node last,Node next){
+            this.key = key;
+            this.value = value;
+            this.last = last;
+            this.next = next;
+        }
     }
 }
 

e961bf2415acb3348c3519135c9181acc2f8b7df
muchen <2921068018@qq.com>
2021-03-15 20:40:33 +0800

    new

--- src/P705设计哈希集合/Solution.java
+++ src/P705设计哈希集合/Solution.java
@@ -1,30 +1,80 @@
 package P705设计哈希集合;
 
+
 class MyHashSet {
-    boolean[] map;
-    /** Initialize your data structure here. */
+    Node[] node;
+    int prim;
     public MyHashSet() {
-        map = new boolean[1000001];
+        prim = 9697;
+        node = new Node[prim];
     }
-
     public void add(int key) {
-        map[key] = true;
+        int prKey = key % prim;
+        if(node[prKey] == null){
+            node[prKey] = new Node(key,null,null);
+        }else{
+            Node t = node[prKey];
+            if(t.key == key){
+                return;
+            }
+            while(t.next != null){
+                t = t.next;
+                if(t.key == key){
+                    return;
+                }
+            }
+            t.next = new Node(key,t,null);
+        }
     }
-
-    public void remove(int key) {
-        map[key] = false;
-    }
-
-    /** Returns true if this set contains the specified element */
     public boolean contains(int key) {
-        return map[key];
+        int prKey = key % prim;
+        Node t = node[prKey];
+        while (t!=null){
+            if(t.key == key){
+                return true;
+            }
+            t = t.next;
+        }
+        return false;
+    }
+    public void remove(int key) {
+        int prKey = key % prim;
+        Node t = node[prKey];
+        if(t == null){
+            return;
+        }
+        if(t.key == key){
+            if(t.next == null){
+                node[prKey] = null;
+                return;
+            }else{
+                t.next.last = null;
+                node[prKey] = t.next;
+                return;
+            }
+        }
+        while (t.next != null){
+            t = t.next;
+            if(t.key == key){
+                t.last.next = t.next;
+                if(t.next != null){
+                    t.next.last = t.last;
+                }
+                return;
+            }
+        }
+    }
+    class Node{
+        int key;
+        Node next;
+        Node last;
+        public Node(){
+
+        }
+        public Node(int key, Node last, Node next){
+            this.key = key;
+            this.last = last;
+            this.next = next;
+        }
     }
 }
-
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet obj = new MyHashSet();
- * obj.add(key);
- * obj.remove(key);
- * boolean param_3 = obj.contains(key);
- */

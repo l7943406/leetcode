c8ea85b66184473becdd782665acaa420951b455
muchen <2921068018@qq.com>
2021-03-24 18:48:42 +0800

    new

--- src/P456132模式/Solution.java
+++ src/P456132模式/Solution.java
@@ -1,27 +1,24 @@
 package P456132模式;
 
+import java.util.Stack;
+
 class Solution {
     public boolean find132pattern(int[] nums) {
         if(nums.length < 3){
             return false;
         }
-        int[] left = new int[nums.length];
-        int leftNum = nums[0];
-        for (int i = 0; i < nums.length - 1; i++) {
-            if(nums[i] < leftNum){
-                leftNum = nums[i];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(nums[nums.length - 1]);
+        int num2 = Integer.MIN_VALUE;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if(nums[i] < num2){
+                return true;
             }
-
-            left[i + 1] = leftNum;
-
-        }
-        for (int i = 1; i < nums.length - 1; i++) {
-            if(nums[i] > left[i] && nums[i] - left[i] > 1){
-                for (int j = i + 1; j < nums.length; j++) {
-                    if(nums[j] < nums[i] && nums[j] > left[i]){
-                        return true;
-                    }
-                }
+            while (!stack.isEmpty() && stack.peek() < nums[i]){
+                num2 = stack.pop();
+            }
+            if(nums[i] > num2) {
+                stack.push(nums[i]);
             }
         }
         return false;

ec526bdb50a2bc233339f4503604d739ab0e8fec
muchen <2921068018@qq.com>
2021-04-07 15:45:53 +0800

    new

--- src/P81搜索旋转排序数组II/Solution.java
+++ src/P81搜索旋转排序数组II/Solution.java
@@ -1,10 +1,21 @@
 package P81搜索旋转排序数组II;
 
 import java.util.Arrays;
-
+/*
 class Solution {
     public boolean search(int[] nums, int target) {
         Arrays.sort(nums);
         return Arrays.binarySearch(nums,target) >= 0;
+    }
+}
+*/
+class Solution {
+    public boolean search(int[] nums, int target) {
+        for(int i : nums){
+            if(i == target){
+                return true;
+            }
+        }
+        return false;
     }
 }

fc3af7a91e2145bccab7684ae5adff3db43d0618
muchen <2921068018@qq.com>
2021-04-08 20:46:20 +0800

    new

---
+++ src/P153寻找旋转排序数组中的最小值/Solution.java
@@ -0,0 +1,17 @@
+package P153寻找旋转排序数组中的最小值;
+
+class Solution {
+    public int findMin(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l < r){
+            int mid = (l + r) / 2 ;
+            if(nums[mid] < nums[r]){
+                r = mid;
+            }else{
+                l = mid + 1;
+            }
+        }
+        return nums[r];
+    }
+}

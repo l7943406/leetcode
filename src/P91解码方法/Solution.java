d92623b6b670e1704eb42e114a06bf1927d1e567
muchen <2921068018@qq.com>
2021-04-23 12:36:01 +0800

    new

--- src/P91解码方法/Solution.java
+++ src/P91解码方法/Solution.java
@@ -2,21 +2,18 @@ package P91解码方法;
 
 class Solution {
     public int numDecodings(String s) {
-        if(s.length() == 1){
-            if(s.charAt(0) == '0'){
-                return 0;
-            }
-            return 1;
-        }else if(s.length() < 1){
-            return 0;
-        }
-        if(s.charAt(0) == '0'){
-            return 0;
-        }
         byte[] bytes = s.getBytes();
-        int[] dp = new int[s.length()];
+        int[] dp = new int[bytes.length + 1];
         dp[0] = 1;
+        for (int i = 1; i <= bytes.length; i++) {
+            if(bytes[i - 1] != '0'){
+                dp[i] += dp[i - 1];
+            }
+            if(i > 1 && bytes[i-2]!='0' && (bytes[i - 2] - '0') * 10 + bytes[i - 1] - '0' <= 26){
+                dp[i] += dp[i - 2];
+            }
+        }
 
-        return -1;
+        return dp[bytes.length];
     }
 }

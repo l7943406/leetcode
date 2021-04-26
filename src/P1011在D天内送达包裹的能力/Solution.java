66cd998915915089a61650cc24855733e18c2153
muchen <2921068018@qq.com>
2021-04-26 23:32:23 +0800

    new

---
+++ src/P1011在D天内送达包裹的能力/Solution.java
@@ -0,0 +1,45 @@
+package P1011在D天内送达包裹的能力;
+
+
+class Solution {
+    int[] weights;
+    int D;
+    public int shipWithinDays(int[] weights, int D) {
+        this.weights = weights;
+        this.D = D;
+
+        int sum = 0;
+        int max = 0;
+        for (int weight : weights) {
+            sum += weight;
+            max = Math.max(max,weight);
+        }
+        int l = max;
+        int r = sum;
+        while (l <= r){
+            int mid = (l + r) / 2;
+            if(can(mid)){
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+    public boolean can(int step){
+        int count = 1;
+        int sum = 0;
+        for (int weight : weights) {
+            if (sum + weight > step) {
+                sum = weight;
+                count++;
+                if (count > D) {
+                    return false;
+                }
+            } else {
+                sum += weight;
+            }
+        }
+        return count <= D;
+    }
+}

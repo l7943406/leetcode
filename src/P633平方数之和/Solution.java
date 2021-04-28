f86b1fe0dba4e3c1d79fcd79f66cddf7cd8d385d
muchen <2921068018@qq.com>
2021-04-28 20:41:20 +0800

    new

--- src/P633平方数之和/Solution.java
+++ src/P633平方数之和/Solution.java
@@ -2,7 +2,7 @@ package P633平方数之和;
 
 class Solution {
     public boolean judgeSquareSum(int c) {
-        for(int i = 0; i * i <= c; i++){
+        for(long i = 0; i * i <= c; i++){
             double sqrt = Math.sqrt(c - i * i);
             if((int) sqrt == sqrt){
                 return true;

608d39c09b953effccd50d66c1ebfbb2560b792e
muchen <2921068018@qq.com>
2021-07-20 09:39:48 +0800

    new

--- src/P1877数组中最大数对和的最小值/Solution.java
+++ src/P1877数组中最大数对和的最小值/Solution.java
@@ -4,17 +4,23 @@ import java.util.*;
 
 class Solution {
     public int minPairSum(int[] nums) {
-        int[] sort = new int[100001];
+        int max = Integer.MIN_VALUE;
+        for(int num : nums){
+            max = Math.max(max,num);
+        }
+        int[] sort = new int[max + 1];
         for(int num : nums){
             sort[num] ++;
         }
         int index = 0;
-        for (int i = 0; i < 100001; i++) {
-            for (int j = 0; j < sort[i]; j++) {
-                nums[index++] = i;
+        for (int i = 0; i < sort.length; i++) {
+            if(sort[i] != 0) {
+                for (int j = 0; j < sort[i]; j++) {
+                    nums[index++] = i;
+                }
             }
         }
-        int max = Integer.MIN_VALUE;
+        max = Integer.MIN_VALUE;
         for (int i = 0; i < nums.length / 2; i++) {
             max = Math.max(max,nums[i] + nums[nums.length - i - 1]);
         }

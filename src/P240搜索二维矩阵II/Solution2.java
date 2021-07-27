01f03fb174c2c02c0cd9d67ec53eca28f4e3707f
muchen <2921068018@qq.com>
2021-07-27 10:59:05 +0800

    new

---
+++ src/P240搜索二维矩阵II/Solution2.java
@@ -0,0 +1,23 @@
+package P240搜索二维矩阵II;
+
+@SuppressWarnings("all")
+
+
+/*
+* 搜索对角线
+*
+* */
+class Solution2 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length > matrix[0].length){
+
+        }else{
+
+        }
+
+
+        return false;
+    }
+
+
+}

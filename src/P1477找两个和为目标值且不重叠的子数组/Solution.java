a262dde1b5564fb1d52a483804d3d3181cd9fa64
muchen <2921068018@qq.com>
2021-09-07 10:42:37 +0800

    new

---
+++ src/P1477找两个和为目标值且不重叠的子数组/Solution.java
@@ -0,0 +1,16 @@
+package P1477找两个和为目标值且不重叠的子数组;
+
+
+
+class Solution {
+    public int minSumOfLengths(int[] arr, int target) {
+        return 0;
+    }
+}
+/*
+给你一个整数数组arr 和一个整数值target。
+
+请你在 arr中找 两个互不重叠的子数组且它们的和都等于target。可能会有多种方案，请你返回满足要求的两个子数组长度和的 最小值 。
+
+请返回满足要求的最小长度和，如果无法找到这样的两个子数组，请返回 -1。
+ */
